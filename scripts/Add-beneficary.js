2213cdd3f73d1c4cb9bb90e4d820f47eca575a69
Omphile Mohlala <omphilestudent@gmail.com>
2025-08-06 13:27:08 +0200

    JS Benenificary.

---
+++ scripts/Add-beneficary.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMComnetLoaded', function (){
+    const maincontent = document.querySelector('.main-content');
+
+    if (maincontent) {
+        return;
+
+        const addBeneficaryBtn = `
+        <h2>Add Beneficiary</h2>
+        
+        `
+    }
+} )
