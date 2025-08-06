dd8f26d077af19f758e61b9b216e4a1bf2d5b35c
Omphile Mohlala <omphilestudent@gmail.com>
2025-08-06 15:04:17 +0200

    JS Benenificary.

--- scripts/Add-beneficary.js
+++ scripts/Add-beneficary.js
@@ -1,12 +1,33 @@
-document.addEventListener('DOMComnetLoaded', function (){
-    const maincontent = document.querySelector('.main-content');
+document.addEventListener('DOMContentLoaded', function () {
+    const addBeneficiaryBtn = document.getElementById('addBeneficiaryBtn');
 
-    if (maincontent) {
-        return;
+    if (addBeneficiaryBtn) {
+        addBeneficiaryBtn.addEventListener('click', function () {
+            const mainContent = document.getElementById('mainContent');
 
-        const addBeneficaryBtn = `
-        <h2>Add Beneficiary</h2>
-        
-        `
+            mainContent.innerHTML = `
+                <section class="heading">
+                    <div class="heading-content">
+                        <div class="heading-title">
+                            <h4>Add Beneficiary</h4>
+                        </div>
+                        <div class="details">
+                            <span class="details-title">Beneficiary Name</span>
+                            <span class="details-name">MR O MOHLALA</span>
+                            <span class="details-title">Beneficiary Account</span>
+                            <span class="details-name">1234567890</span>
+                        </div>
+                        <button id="cancelBtn" class="ben" style="margin-top: 20px;">Cancel</button>
+                    </div>
+                </section>
+            `;
+
+            const cancelButton = document.getElementById('cancelBtn');
+            if (cancelButton) {
+                cancelButton.addEventListener('click', function () {
+                    location.reload();
+                });
+            }
+        });
     }
-} )
+});
