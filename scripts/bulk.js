e66d1de82eff165e08b8b6ea7a07683f3e0a9154
Omphile Mohlala <omphilestudent@gmail.com>
2025-08-19 10:22:06 +0200

    bulk payment update

--- scripts/bulk.js
+++ scripts/bulk.js
@@ -31,7 +31,7 @@ document.getElementById('mainContent').innerHTML = `
                 <img src="../images/send-cash-active.svg" style="position: absolute; left: 40px; width: 50px; height: 30px; top: 10px;">
                 <span style="position: absolute; top: 45px; left: 30px">Normal EFT</span>
             </div>
-            <div class="same-day">
+            <div class="same-day" id="sameDay">
                 <img src="../images/same-day.svg" style="position: absolute; left: 40px; width: 50px; height: 30px; top: 10px;">
                 <span style="position: absolute; top: 45px; left: 35px">Same Day</span>
             </div>
@@ -53,11 +53,11 @@ document.getElementById('mainContent').innerHTML = `
 </div>
 <div class="cut-off-popup" id="popup" style="display:none;">
     <div class="overlay"></div>
-    <div class="cut-off-popup-header">Payment types</div>
+    <div class="cut-off-popup-header"><b>Payment types</b></div>
     <div class="cut-off-popup-body">
         <div class="cut-off-popup-eft">
             <img src="../images/info-trans.svg" style="position: absolute; left: 10px; width: 15px; height: 30px; top: 1px; ">
-            <span style="position: absolute; left: 30px;"><b>Normal EFT</b></span>
+            <span style="position: absolute; left: 30px;"><b>Normal EFT</b> - R2.00 transaction fee will be changed for each beneficairy</span>
             <h3 style="position: absolute; top: 50px;">Cut-off times</h3>
             <ul>
             <li>Mon-Fri: 16:00 (transactions up to R5 million)</li>
@@ -66,8 +66,41 @@ document.getElementById('mainContent').innerHTML = `
             <li>Capitec to Capitec payments will be processed immediately</li>
 </ul>
         </div>
+        <div class="cut-off-popup-eft" >
+            <img src="../images/info-trans.svg" style="position: absolute; left: 10px; width: 15px; height: 30px; top: 1px; ">
+            <span style="position: absolute; left: 30px;"><b>Immediate</b> - R6.00 transactions fee will be charged for each beneficiary</span>
+            <h3 style="position: absolute; top: 50px;">Cut-off times</h3>
+            <ul>
+            <li>Mon-Fri: 06:00 - 16:00 (transactions up to R5 million)</li>
+            <li>Mon-Fri: 16:00 - 20:00(transactions up to R250 000)</li>
+            <li>Weekends and public holidays: 06:00 - 18:00 (transactions up to R250 000)</li>
+            <li>Immediate payment cannot be stopped reversed or cancelled</li>
+            <li>Only payment to RTC participating banks can be processed immediately</li>
+</ul>
+        </div>
+         <div class="cut-off-popup-eft">
+            <img src="../images/info-trans.svg" style="position: absolute; left: 10px; width: 15px; height: 30px; top: 1px; ">
+            <span style="position: absolute; left: 30px;"><b>Same day </b>- Payments above R5 million be processed the same day. A transaction fee of R40.00 will be charged.</span>
+            <h3 style="position: absolute; top: 50px;">Cut-off times</h3>
+            <ul>
+            <li>Mon-Fri: 15:30 (transactions up to R5 million)</li>
+            <li>Saturday: Services not available</li>
+            <li>Sunday, public holidays or after cut-times, payments will be processed the next business day</li>
+           </ul>
+        </div>
+        <div class="cut-off-popup-eft">
+            <img src="../images/info-trans.svg" style="position: absolute; left: 10px; width: 15px; height: 30px; top: 1px; ">
+            <span style="position: absolute; left: 30px;"><b>SARS efiling </b>- A R10.00 transaction fee will be charged.</span>
+            <h3 style="position: absolute; top: 50px;">Cut-off times</h3>
+            <ul>
+            <li>Mon-Fri: 15:30 (transactions up to R5 million)</li>
+            <li>Saturday: Services not available</li>
+            <li>Sunday, public holidays or after cut-times, payments will be processed the next business day</li>
+           </ul>
+        </div>
     </div>
-    <div class="okay" id="okay">Okay</div>
+    <div class="line" style="position: absolute; border-top: 1px solid #cccccc; bottom: 55px; width: 100%"></div>
+    <div class="okay" id="okay" >Okay</div>
 </div>
 <div class="letsgo">
     <button class="cancel">Cancel</button>
@@ -208,3 +241,119 @@ function handleFiles(files) {
         alert('Invalid file type. Please upload a CSV or TXT file.');
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sameDayBtn = document.getElementById('sameDay');
+    sameDayBtn.addEventListener('click', function() {
+        document.getElementById('sameDay').innerHTML = `
+            <div class="content-header">
+            <h1 style="position: absolute; top: 50px; text-wrap: nowrap;">Bulk Payment</h1>
+    </div>
+        <div class="disclaimer-box" id="disclaimerBox">
+            <div class="payment-header">
+                <h2>Payment details</h2>
+                <div class="left">
+                    <div class="upper-left">
+                        <select class="uu">
+                            <option>From account</option>
+                            <option>
+                                <div class="account-number" style="position: absolute;"><img src="../images/transact.svg" alt="transact">105 2626 4558</div>
+                            </option>
+                        </select>
+                        <input type="text" placeholder="Your reference" class="oo">
+                    </div>
+                    <div class="lower-left">
+                        <h4>Show in transaction history as</h4>
+                        <span class="radio-group">
+                    <input type="radio" name="paymentType" id="one">
+                    <label for="one">One payment</label>
+                    <input type="radio" name="paymentType" id="separate">
+                    <label for="separate">Separate Payments</label>
+                </span>
+                    </div>
+                </div>
+                <div class="rights">
+                    <h4 style="position: absolute; text-wrap: nowrap; left: -160px; top: -10px; font-weight: lighter;">Payment type</h4>
+                    <div class="eft">
+                        <img src="../images/send-cash-active.svg" style="position: absolute; left: 40px; width: 50px; height: 30px; top: 10px;">
+                            <span style="position: absolute; top: 45px; left: 30px">Normal EFT</span>
+                    </div>
+                    <div class="same-day action" id="sameDay">
+                        <img src="../images/same-day.svg" style="position: absolute; left: 40px; width: 50px; height: 30px; top: 10px;">
+                            <span style="position: absolute; top: 45px; left: 35px">Same Day</span>
+                    </div>
+                    <div class="cut-off">
+                        <img src="../images/info-trans.svg" style="position: absolute; left: 10px; width: 15px; height: 30px; top: 5px;">
+                            <span style="position: absolute; left: 30px; top: 15px; font-size: 0.6rem;">Transaction fee:</span>
+                            <div class="cut-off-box">
+                                <span><b>EFT</b> - R2.00 per beneficiary</span>
+                                <span><b>Immediate</b> - R6.00 per beneficiary</span>
+                                <span><b>Same day</b> - R40.00 per beneficiary</span>
+                                <span><b>Capitec beneficiaries</b> - R1.00</span>
+                                <span><b>SARS eFiling</b> - R10.00</span>
+                                <span style="color: #00aeff; cursor: pointer;" id="cut-off-popup">Cut-off and available times</span>
+                            </div>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+        <div class="cut-off-popup" id="popup" style="display:none;">
+            <div class="overlay"></div>
+            <div class="cut-off-popup-header"><b>Payment types</b></div>
+            <div class="cut-off-popup-body">
+                <div class="cut-off-popup-eft">
+                    <img src="../images/info-trans.svg" style="position: absolute; left: 10px; width: 15px; height: 30px; top: 1px; ">
+                        <span style="position: absolute; left: 30px;"><b>Normal EFT</b> - R2.00 transaction fee will be changed for each beneficairy</span>
+                        <h3 style="position: absolute; top: 50px;">Cut-off times</h3>
+                        <ul>
+                            <li>Mon-Fri: 16:00 (transactions up to R5 million)</li>
+                            <li>Saturday: 10:15 (transactions up to R5 million)</li>
+                            <li>Sunday, public holidays or after cut-times, payments will be processed the next business day</li>
+                            <li>Capitec to Capitec payments will be processed immediately</li>
+                        </ul>
+                </div>
+                <div class="cut-off-popup-eft" >
+                    <img src="../images/info-trans.svg" style="position: absolute; left: 10px; width: 15px; height: 30px; top: 1px; ">
+                        <span style="position: absolute; left: 30px;"><b>Immediate</b> - R6.00 transactions fee will be charged for each beneficiary</span>
+                        <h3 style="position: absolute; top: 50px;">Cut-off times</h3>
+                        <ul>
+                            <li>Mon-Fri: 06:00 - 16:00 (transactions up to R5 million)</li>
+                            <li>Mon-Fri: 16:00 - 20:00(transactions up to R250 000)</li>
+                            <li>Weekends and public holidays: 06:00 - 18:00 (transactions up to R250 000)</li>
+                            <li>Immediate payment cannot be stopped reversed or cancelled</li>
+                            <li>Only payment to RTC participating banks can be processed immediately</li>
+                        </ul>
+                </div>
+                <div class="cut-off-popup-eft">
+                    <img src="../images/info-trans.svg" style="position: absolute; left: 10px; width: 15px; height: 30px; top: 1px; ">
+                        <span style="position: absolute; left: 30px;"><b>Same day </b>- Payments above R5 million be processed the same day. A transaction fee of R40.00 will be charged.</span>
+                        <h3 style="position: absolute; top: 50px;">Cut-off times</h3>
+                        <ul>
+                            <li>Mon-Fri: 15:30 (transactions up to R5 million)</li>
+                            <li>Saturday: Services not available</li>
+                            <li>Sunday, public holidays or after cut-times, payments will be processed the next business day</li>
+                        </ul>
+                </div>
+                <div class="cut-off-popup-eft">
+                    <img src="../images/info-trans.svg" style="position: absolute; left: 10px; width: 15px; height: 30px; top: 1px; ">
+                        <span style="position: absolute; left: 30px;"><b>SARS efiling </b>- A R10.00 transaction fee will be charged.</span>
+                        <h3 style="position: absolute; top: 50px;">Cut-off times</h3>
+                        <ul>
+                            <li>Mon-Fri: 15:30 (transactions up to R5 million)</li>
+                            <li>Saturday: Services not available</li>
+                            <li>Sunday, public holidays or after cut-times, payments will be processed the next business day</li>
+                        </ul>
+                </div>
+            </div>
+            <div class="line" style="position: absolute; border-top: 1px solid #cccccc; bottom: 55px; width: 100%"></div>
+            <div class="okay" id="okay" >Okay</div>
+        </div>
+        <div class="letsgo">
+            <button class="cancel">Cancel</button>
+            <button class="next" id="nextBtn">Continue</button>
+        </div>
+
+    `
+    });
+})
